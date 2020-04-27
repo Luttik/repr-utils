c514089904da5ed4687a60a445f011b4e602ef29
luttik <dtluttik@gmail.com>
2020-04-27 17:16:36 +0200

    Added locking tests for markdown.

--- test/test_table.py
+++ test/test_table.py
@@ -1,14 +1,11 @@
 from repr_utils import Table
+from test.fixtures import ReprLocker
 
 
-def test_table():
+def test_table(repr_locker: ReprLocker):
     table = Table(
         ["a", "bla", "code & stuff"],
         [["a", 2, 3], ["oh man", 2, 3, 4], ["last row", 4, 5, 6]],
         has_index=True,
     )
-    print(table._repr_html_())
-    assert str(table) is not None
-    assert table._repr_html_() is not None
-    assert table._repr_markdown_() is not None
-    assert table._repr_latex_() is not None
+    repr_locker.lock(table)
